0e364180d12c048180ecf7b04d6092dbec6b32e5
Marjan <marjan.mubarok2012@gmail.com>
2018-06-17 17:42:28 +0100

    ex39 and before.

--- Python3_Hardway/ex23.py
+++ Python3_Hardway/ex23.py
@@ -1,6 +1,7 @@
 imports sys
 script, encoding, error = sys.argv
 
+
 def main(language_file, encoding, errors):
     line = languages_file.readlines()
 
@@ -8,12 +9,14 @@ def main(language_file, encoding, errors):
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
 
+
 def print_line(line, encoding, errors):
     next_lang = line.strip()
     raw_bytes = next_lang.encode(encoding, errors=errors)
     cooked_string = raw_bytes.decode(encoding, errors)
 
     print(raw_bytes, "<===>", cooked_string)
+
 
 languages = open("languages.txt", encodings="utf-8")
 
